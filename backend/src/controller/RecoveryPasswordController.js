28dfe92cec7b1ab9414210d36047260833869736
Edgar-PTC <20230280@ricaldone.edu.sv>
2026-04-25 12:54:15 -0600

    feat: Ultimo commit

--- backend/src/controller/RecoveryPasswordController.js
+++ backend/src/controller/RecoveryPasswordController.js
@@ -7,12 +7,72 @@ import { config } from "../config.js";
 
 import Estudiantes from "../models/Estudiantes.js";
 import Docentes from "../models/Docentes.js";
+import HTMLRecoveryEmail from "../utils/HTMLRecoveryEmail.js"
 
 const recoveryPassword = {}
 
 recoveryPassword.sendEmail = async (req, res) => {
     try {
-        
+        const { email } = req.body;
+
+        //Validamos que el correo existe
+        let userFound = await Estudiantes.findOne({email});
+        let TypeRole;
+        if(!userFound){
+            //Si no existe buscamos en los docentes
+            userFound = await Docentes.findOne({email});
+            if(!userFound){
+                return res.status(404).json({message: "user not found"})
+            }
+            TypeRole = "Docente"
+        }
+        TypeRole = "Estudiante"
+
+
+        //Generar el numero aleatorio
+        const randomCode = crypto.randomBytes(3).toString("hex");
+
+        //Guardamos en un token
+        const token = jsonwebtoken.sign(
+            //Que se guarda
+            {email, randomCode, TypeRole, verified: false},
+
+            //Llave secreta
+            config.jwt.secret,
+
+            //Cuando expira
+            {expiresIn: "15m"}
+        );
+
+        res.cookie("recoveryCookie", token, {maxAge: 15 * 60 * 1000});
+
+        //Enviar por correo
+        //1. quien lo envia
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: config.email.user_email,
+                pass: config.email.user_password
+            }
+        })
+
+        //2. Que se envia
+        const mailOptions = {
+            from: config.email.user_email,
+            to: email,
+            subject: `${randomCode}. Codigo de recuperación de contraseña`,
+            html: HTMLRecoveryEmail(randomCode, userFound.name, userFound.lastName)
+        }
+
+        //3 enviar
+        transporter.sendMail(mailOptions, (error, info) =>{
+            if(error){
+                console.log(error)
+                return res.status(500).json({message: "error al enviar el correo"})
+            }
+        });
+
+        return res.status(200).json({message: "email sent"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -21,7 +81,29 @@ recoveryPassword.sendEmail = async (req, res) => {
 
 recoveryPassword.verifyCode = async(req, res) => {
     try {
+        //1- solicitamos el codigo
+        const { code } = req.body;
         
+        //2- obtenemos codigo en cookie
+        const token = req.cookies.recoveryCookie
+
+        //3- extraer token
+        const decoded = jsonwebtoken.verify(token, config.jwt.secret);
+
+        //4- comparar
+        if(code !== decoded.randomCode){
+            return res.status(400).json({message: "Invalid code"})
+        }
+
+        const newToken = jsonwebtoken.sign(
+            {email: decoded.email, TypeRole: decoded.TypeRole, verified: true},
+            config.jwt.secret,
+            {expiresIn: "15m"}
+        )
+
+        res.cookie("recoveryCookie", newToken, { maxAge: 15 * 60 * 1000 });
+
+        return res.status(200).json({ message: "Code verified sucessfully" })
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
@@ -30,7 +112,43 @@ recoveryPassword.verifyCode = async(req, res) => {
 
 recoveryPassword.updatePassword = async(req, res) => {
     try {
-        
+        //1. Solicito los datos
+        const { newPassword, comfirmedPassword } = req.body;
+
+        //Comparar
+        if(newPassword !== comfirmedPassword){
+            return res.status(400).json({message: "Passwords doesn't match"})
+        }
+
+        //vamos a comprobar que el token ya esta verificado
+        const token = req.cookies.recoveryCookie;
+        const decoded = jsonwebtoken.verify(token, config.jwt.secret)
+
+        if(!decoded.verified){
+            return res.status(400).json({message: "code not verified"})
+        }
+
+        //Encriptar la contra
+        const passwordHash = await bcrypts.hash(newPassword, 10);
+
+        //Dependiendo del TypeRole se actualiza
+        if(decoded.TypeRole == "Estudiante"){
+            await Estudiantes.findOneAndUpdate(
+                {email: decoded.email},
+                {password: passwordHash},
+                {new: true}
+            )
+        }else if(decoded.TypeRole == "Docente"){
+            await Docentes.findOneAndUpdate(
+                {email: decoded.email},
+                {password: passwordHash},
+                {new: true}
+            )
+        }
+
+        res.clearCookie("recoveryCookie");
+
+        return res.status(200).json({message: "Password updated"})
     } catch (error) {
         console.log("Error: " + error);
         return res.status(500).json({message: "Internal server error"});
--- backend/src/utils/HTMLRecoveryEmail.js
+++ backend/src/utils/HTMLRecoveryEmail.js
@@ -1,4 +1,4 @@
-const HTMLRegisterEmail = (Code, name, lastname) => {
+const HTMLRecoveryEmail = (Code, name, lastname) => {
     // Generar cada dígito como celda de tabla
     const codeDigits = [...Code].map(letter => `
         <td style="background-color: rgba(47, 55, 61, 0.269); padding: 15px 20px; border-radius: 20px; font-size: 50px; text-align: center; font-weight: bold;">
@@ -13,7 +13,7 @@ const HTMLRegisterEmail = (Code, name, lastname) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
             </head>
             <body style="margin: 0; font-family: Arial, sans-serif;">
-                <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #456cb0;">
+                <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #286e5a;">
                     <tr>
                         <td align="center" style="padding: 20px;">
                             <table width="100%" max-width="600" cellpadding="0" cellspacing="0" border="0" style="max-width: 600px; width: 100%; background-color: aliceblue; border-radius: 30px; padding: 20px;">
@@ -22,12 +22,10 @@ const HTMLRegisterEmail = (Code, name, lastname) => {
                                         <h2 style="font-size: 32px; margin: 0 0 20px 0; color: #333;">Código de Verificación</h2>
                                         
                                         <p style="font-size: 16px; line-height: 1.5; color: #555; margin: 0 0 30px 0;">
-                                            Hola ${name} ${lastname}. Gracias por tu interés en nuestra institución.<br>
-                                            Estamos a punto de terminar, solo debemos verificar tu correo.<br>
-                                            Ingresa el siguiente código para completar tu registro:
-                                        </p>
-                                        
-                                        <!-- Tabla del código (horizontal) -->
+                                            Hola ${name} ${lastname}.<br>
+                                            Estamos a punto de terminar con el proceso de recuperación de contraseña, solo debemos verificar tu correo.<br>
+                                            Ingresa el siguiente código para completar el proceso:
+                                        </p>                                    
                                         <table cellpadding="0" cellspacing="0" border="0" align="center" style="margin: 30px auto;">
                                             <tr style="display: flex; flex-direction: row; gap: 5px">
                                                 ${codeDigits}
@@ -48,4 +46,4 @@ const HTMLRegisterEmail = (Code, name, lastname) => {
     `;
 }
 
-export default HTMLRegisterEmail;
+export default HTMLRecoveryEmail;
